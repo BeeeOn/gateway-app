9daf7095a07ed84af0d66dd0087d2c24e85ed0ed
Martin Sakin <xsakin00@stud.fit.vubr.cz>
2016-02-17 15:19:42 +0100

    bluetooth: full control over MQTT (ready for AdaApp)
    [tomas@novotny.cz: fix typos]

--- tools/bluetooth.py
+++ tools/bluetooth.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 # Author BeeeOn team
 # Date: 2016-02-06
 # Description: Script scans for available Bluetooth devices
@@ -12,13 +12,14 @@ import os,sys
 import subprocess
 from subprocess import Popen, PIPE
 import re
+import SocketServer
 
-devices_file = "bluedevices"	# path to file with list of MAC bluetooth devices
+devices_file = ".bluedevices"	# path to file with list of MAC bluetooth devices
 openhab_path = "/opt/openhab"	# path to OpenHAB (where is ./start.sh)
 openhab_port = 8080				# port http
-mqtt_canal = "BeeeOn/openhab"	# name mosquitto chanel
-sleep_file = 2					# X seconds for check changes "devices_file"
+mqtt_channel = "BeeeOn/openhab"	# name mosquitto channel
 sleep_scan = 9					# X seconds for bluetooth scan
+HOST, PORT = "localhost", 9871	# http handler and port
 log = False						# continuous list-ing to stdout
 
 #=======================================================================
@@ -27,28 +28,25 @@ know = None
 #=======================================================================
 class Knowledge(object):	# work with and memory MAC addresses
 	def __init__(self):
-		self.iii = 0
-		self.listMAC = []
-		self.name = []
-		self.item = []
-		self.change = False
+		self.listMAC = []	# a list of MAC addresses
+		self.item = []		# mark in OH config
 
 	def addMAC(self, mac):
 		if mac in self.listMAC:
 			return
-		self.iii = self.iii + 1
 		self.listMAC.append(mac)
-		self.name.append('')
 		self.item.append('')
-		self.change = True
+		self.writeOH()		# edit in OH conf
+		self.savetofile()	# save MAC
 
 	def removeMAC(self, mac):
+		if not mac in self.listMAC:
+			return
 		idx = self.listMAC.index(mac)
 		del self.listMAC[idx]
-		del self.name[idx]
 		del self.item[idx]
-		self.iii = self.iii - 1
-		self.change = True
+		# not necessary edit in OH, just will be not used
+		self.savetofile()	# remove MAC
 
 	def addItem(self, mac, item):
 		idx = self.listMAC.index(mac)
@@ -60,114 +58,144 @@ class Knowledge(object):	# work with and memory MAC addresses
 			return self.item[idx]
 		else: return ''
 
-	def addName(self, mac, name):
-		if mac in self.listMAC:
-			idx = self.listMAC.index(mac)
-			self.name[idx] = name
-
-	def getName(self, mac):
-		if mac in self.listMAC:
-			idx = self.listMAC.index(mac)
-			return self.name[idx]
-		else: return ''
-
-	def checkChange(self, newlist):
-		different = False
-		if len(newlist) != len(self.listMAC):
-			different = True
-		elif len(set(newlist) & set(self.listMAC)) != len(newlist):
-			different = True
-
-		if different:
-			for mac in self.listMAC:
-				if not mac in newlist:
-					self.removeMAC(mac)
-			for mac in newlist:
-				if not mac in self.listMAC:
-					self.addMAC(mac)
-
-	def ischange(self):
-		pom = self.change
-		self.change = False
-		return pom
-
 	def dbg(self):		# debug print
-		for i in range (0, self.iii):
-			print self.listMAC[i], self.name[i], self.item[i]
+		for i in range (0, len(self.listMAC)):
+			print self.listMAC[i], self.item[i]
+
+	def isstored(self,mac):
+		if mac in self.listMAC:
+			return True
+		else:
+			return False
+
+	def writeOH(self):	# write changes to config files OpenHab
+		f_items = open(openhab_path+"/configurations/items/bluetooth.items", 'w')	# items
+		f_rules = open(openhab_path+"/configurations/rules/bluetooth.rules", 'w')	# rules
+		f_items.write('Group All\nString fromPy (All)\n')
+		f_items.write('Switch toMqtt (All) {mqtt=">[broker:BeeeOn/openhab:command:on:${command}],>[broker:BeeeOn/openhab:command:*:${command}]"}\n')
+		f_items.write('String fromMqtt (All) {mqtt="<[broker:BeeeOn/openhab/bt:state:default]"}\n')
+		f_rules.write('rule "commands from Py to Mqtt"\nwhen\n\tItem fromPy received update\nthen\n\ttoMqtt.sendCommand(fromPy.state)\nend\n\n')
+		f_rules.write('rule "commands from Mqtt to Py"\nwhen\n\tItem fromMqtt received update\nthen\n\tsendHttpPutRequest("http://'+HOST+':'+str(PORT)+'", "text/plain", fromMqtt.state.toString)\nend\n')
+		for idx, mac in enumerate(know.listMAC):
+			index = str(idx)
+			f_items.write('\nSwitch BT'+index+' (All) {bluetooth="'+mac+'"}\n')
+			know.addItem(mac, ('BT'+ index) )
+			f_items.write('Switch MQTT'+index+' (All) {mqtt=">[broker:'+mqtt_channel+':command:on:euid;'+ mac +';device_id;5;module_id;0x00;value;1.00],>[broker:'+mqtt_channel+':command:off:euid;'+ mac +';device_id;5;module_id;0x00;value;2.00]"}\n')
+
+			f_rules.write('\nrule "rule '+index+' for BT'+index+'"\nwhen\n')
+			f_rules.write('\tItem BT'+index+' received update\nthen\n')
+			f_rules.write('\tMQTT'+index+'.sendCommand(BT'+index+'.state)\nend\n')
+		f_items.close()
+		f_rules.close()
+
+	def savetofile(self):
+		head = '# List of saved MAC address devices for OpenHAB\n# Read only at start. Please do not modify!\n'
+
+		with open(devices_file, 'w') as fi:	# touch file
+			fi.write(head)
+			for mac in know.listMAC:
+				fi.write(mac + '\n')
+
+#=======================================================================
+class TCPHandler(SocketServer.BaseRequestHandler):
+	def handle(self):
+		global know
+		self.data = self.request.recv(1024).strip()
+		dat = str(self.data).split()
+		mesg = dat[-1]
+		if mesg == '': return
+		numOfTry = 0
+		output = ''
+		self.request.sendall("200 OK\r\n")
+		if mesg == 'test':
+			if log: print('TCPHandler, just test')
+
+		if mesg == 'iwannalistofbluetooth':
+			if log: print('OH wants list of bluetooth - I do scanning...')
+			while numOfTry < 8:		# number of experiments, when the scan fails (BT is busy)
+				try:
+					numOfTry += 1
+					output = subprocess.check_output(["hcitool", "scan"]) # do full scan
+				except:
+					time.sleep(1)
+					continue
+
+			if output == '':
+				#self.request.sendall("400 FAIL\r\n")
+				return
+			output = output.split('\n')
+
+			for line in output:
+				if line.find(':') > 0:
+					lili = line.split()
+					if log: print(lili)
+					if len(lili) == 2 and not know.isstored(converse(lili[0])):
+						strforsend = 'euid;'+ converse(lili[0]) +';device_id;5;module_id;0x00;value;0.00;name;' + lili[1]
+						if log: print(strforsend)
+						try: os.system('curl --max-time 2 --connect-timeout 2 --header "Content-Type: text/plain" --request PUT --data "'+strforsend+'" http://localhost:'+str(openhab_port)+'/rest/items/fromPy/state')
+						except: print(str(sys.argv[0]) + ": Failed sending2 to OpenHAB")
+
+		mesg = mesg.split(';')	# have to parse first !
+
+		if mesg[0] == 'adddevicetolist':		# example: adddevicetolist;00ABCDEF0123
+			if log: print('ADD DEVICE: ' + mesg[1])
+			know.addMAC(mesg[1])		# mesg[1] # there is a mac to add
+		if mesg[0] == 'removedevicefromlist':
+			if log: print('REMOVE DEVICE: ' + mesg[1])
+			know.removeMAC(mesg[1])
 
 #=======================================================================
 def failBT():	# print error and The Bad End
-	print(str(sys.argv[0]) + ': BT scan failed! Maybe BT dongle is not connected.')
+	print(str(sys.argv[0]) + ': BT scan failed! Maybe BT donge is not connected.')
 	thread_bool = False
 	sys.exit(1)
 
 #=======================================================================
-def converse(mac):	# addition ":"
+def converse(mac):	# addition or remove ":"
 	MAC = ''
-	for i in range(0, len(mac)):
-		MAC += mac[i]
-		if ((i+1)%2) == 0 and i < len(mac)-1:
-			MAC += ':'
+	if mac.find(':') > 0:	# found ':' so remove it
+		for i in range(0, len(mac)):
+			if mac[i] != ':':
+				MAC += mac[i]
+	else:	# not found ':' so add it
+		for i in range(0, len(mac)):
+			MAC += mac[i]
+			if ((i+1)%2) == 0 and i < len(mac)-1:
+				MAC += ':'
 	return MAC
 
 #=======================================================================
-def check_file():	# control file with MAC
-	global openhab_path, know
-	timestamp = 0
-	templist = []
+def load_file():
+	global know
+	if not os.path.isfile(devices_file):	# if file does not exist, nothing to do
+		return
 
-	if not os.path.isfile(devices_file):	# if file does not exist, create
-		with open(devices_file, 'w') as fi:	# touch file
-			fi.write('# List of MAC address devices for OpenHAB\n# Example:\n# 00:FF:AB:CD:EF:GH\n# 00FFABCDEFGH\n')
-			fi.close()
-
-	while thread_bool:	# over and over
-		if timestamp == os.stat(devices_file).st_mtime:	# file modification time
-			time.sleep(sleep_file)	# sleep
+	f = open(devices_file, 'r')		# read list od MAC
+	for line in f:
+		line = line.strip()		# remove spaces (l+r side)
+		if len(line) < 12:		# less than 12 isn't MAC (+ '\n')
 			continue
-
-		f = open(devices_file, 'r')		# read list od MAC
-		templist = []		# temporary list
-		for line in f:
+		line = line.replace('\t', '')	# remove white spaces
+		line = line.replace(' ', '')
+		line = line.replace(':', '')
+		line = line.replace('\n', '')
+		if line.find('#') > -1:			# if is finds a comment
+			line = line[0:line.find('#')]	# trimming
+		if len(line) > 0:
+			line = line.upper()
 			line = line.strip()
-			if len(line) < 13:	# less than 12 isn't MAC (+ '\n')
-				continue
-			line = line.replace('\t', '')
-			line = line.replace(' ', '')
-			line = line.replace(':', '')
-			line = line.replace('\n', '')
-			if line.find('#') > -1:
-				line = line[0:line.find('#')]	# trimming
-			if len(line) > 0:
-				line = line.upper()
-				line = line.strip()
-				templist.append(line)
-		f.close()
-		timestamp = os.stat(devices_file).st_mtime	# set time last change
+			know.addMAC(line)
+	f.close()
+	if log:
+		text = 'Loaded:'
+		for mac in know.listMAC:
+			text += ' | ' + mac
+		print(text)
 
-		know.checkChange(templist)	# monitoring changes in the list
-
-		if log: print "listMAC: ",templist
-
-		if not know.ischange():	continue	# to begin if lists are same
-
-		# write to config files OpenHab
-		if openhab_path[-1] == '/': openhab_path = openhab_path[0:-1]	# delete slash
-		f_items = open(openhab_path+"/configurations/items/adaapp.items", 'w')
-		f_rules = open(openhab_path+"/configurations/rules/adaapp.rules", 'w')
-		f_items.write('Group All\n')
-		for idx, mac in enumerate(know.listMAC):
-			f_items.write('\nSwitch BT'+ str(idx) +' (All) {bluetooth="'+ mac +'"}\n')
-			know.addItem(mac, ('BT'+ str(idx)) )
-			f_items.write('Switch MQTT'+ str(idx) +' (All) {mqtt=">[broker:'+mqtt_canal+':command:on:euid;'+ mac +';device_id;5;module_id;0x00;value;1.00],>[broker:'+mqtt_canal+':command:off:euid;'+ mac +';device_id;5;module_id;0x00;value;2.00]"}\n')
-
-			for boo in ['ON','OFF']:
-				f_rules.write('\nrule "rule '+ str(idx) +' for mqtt '+ boo +'"\nwhen\n')
-				f_rules.write('\tItem BT'+ str(idx) +' received update '+ boo +'\nthen\n')
-				f_rules.write('\tMQTT'+ str(idx) +'.sendCommand('+ boo +')\nend\n')
-		f_items.close()
-		f_rules.close()
-	#while
+#=======================================================================
+def http_handler():
+	server = SocketServer.TCPServer((HOST, PORT), TCPHandler)
+	server.serve_forever()
 
 #=======================================================================
 def scanBT():		# scanning of individual devices
@@ -191,7 +219,12 @@ def scanBT():		# scanning of individual devices
 
 #=======================================================================
 def main():
-	global log, know
+	global openhab_path, log, know
+
+	if len(sys.argv) == 2:
+		if sys.argv[1] == '--log' or sys.argv[1] == '-l':
+			log = True
+
 	out = 0
 	try: out = subprocess.call(["hciconfig", "hci0", "up"])	# turn on bluetooth dongle
 	except: failBT()		# fail of turn on
@@ -199,16 +232,17 @@ def main():
 
 	know = Knowledge()	# object for parse and memory MAC address
 
+	if openhab_path[-1] == '/': openhab_path = openhab_path[0:-1]	# delete slash
+
+	load_file()		# load stored MAC
+
 	try:
-		thread.start_new_thread( check_file, () )	# check file with list of MAC
-		print(str(sys.argv[0]) + ": OK, run.")
+		#thread.start_new_thread( checking_file, () )	# check file with list of MAC
+		thread.start_new_thread( http_handler, () )	# run http handler
+		print(str(sys.argv[0]) + ": OK, run")
 	except:
 		sys.stderr.write(str(sys.argv[0]) + ": Thread cannot start\n")
 		sys.exit(0)
-
-	if len(sys.argv) == 2:
-		if sys.argv[1] == '--log' or sys.argv[1] == '-l':
-			log = True
 
 	scanBT()
 
