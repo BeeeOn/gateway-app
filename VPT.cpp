BeeeOn/gateway-app

commit a4524712c6b5b72b87b202522269295fe2d56241

euid: extract data type euid_t
Currently, EUID is represented by long long int. This makes the code
unreadable as it is not clear which (semantic) type we are currently
working with as there may be other IDs with different semantics.

VPT.cpp
@@ -101,7 +101,7 @@ VPTSensor::VPTSensor(IOTMessage _msg, shared_ptr<Aggregator> _agg) :
 	http_client.reset(new HTTPClient);
 }
 
-void VPTSensor::fetchAndSendMessage(map<long long int, VPTDevice>::iterator &device)
+void VPTSensor::fetchAndSendMessage(map<euid_t, VPTDevice>::iterator &device)
 {
 	try {
 		pair<bool, Command> response;
@@ -194,16 +194,16 @@ string VPTSensor::buildPasswordHash(std::string content) {
 	return hash;
 }
 
-long long int VPTSensor::parseDeviceId(string &content)
+euid_t VPTSensor::parseDeviceId(string &content)
 {
 	string id = json->getParameterValuesFromContent("id", content);
-	long long int id32 = stoul(id.substr(5,11), nullptr, 16);
-	return VPT_ID_PREFIX | (id32 & VPT_ID_MASK);
+	euid_t euid = stoull(id.substr(5,11), nullptr, 0);
+	return VPT_ID_PREFIX | (euid & VPT_ID_MASK);
 }
 
 void VPTSensor::detectDevices(void) {
 	log.notice("VPT: Start device discovery");
-	long long int id;
+	euid_t id;
 	vector<string> devices = http_client->discoverDevices();
 	VPTDevice device;
 	for (vector<string>::iterator it = devices.begin(); it != devices.end(); it++) {
@@ -227,11 +227,11 @@ void VPTSensor::detectDevices(void) {
 	log.notice("VPT: Stop device discovery");
 }
 
-bool VPTSensor::isVPTSensor(long long int euid) {
+bool VPTSensor::isVPTSensor(euid_t euid) {
 	return map_devices.find(euid) != map_devices.end();
 }
 
-void VPTSensor::updateDeviceWakeUp(long long int euid, unsigned int time)
+void VPTSensor::updateDeviceWakeUp(euid_t euid, unsigned int time)
 {
 	auto it = map_devices.find(euid);
 	if (it == map_devices.end()) {
@@ -242,11 +242,12 @@ void VPTSensor::updateDeviceWakeUp(long long int euid, unsigned int time)
 	VPTDevice &dev = it->second;
 
 	if (time < VPT_DEFAULT_WAKEUP_TIME)
-		dev.wake_up_time = VPT_DEFAULT_WAKEUP_TIME;
-	else
-		dev.wake_up_time = time;
+		time = VPT_DEFAULT_WAKEUP_TIME;
 
+	dev.wake_up_time = time;
 	dev.time_left = dev.wake_up_time;
+
+	log.debug("VPT: Update resfresh time for device " + to_string(euid) + " on " + to_string(time) + " seconds");
 }
 
 void VPTSensor::processCmdSet(Command cmd)

Aggregator.cpp
@@ -426,13 +426,12 @@ void Aggregator::loadCache() {
 			continue;
 
 		StringTokenizer token(line, (std::string)";", StringTokenizer::TOK_TRIM | StringTokenizer::TOK_IGNORE_EMPTY);
-
 		for (unsigned int i = 0; i < (unsigned int)token.count();) {
 			if (token[i].compare("time") == 0)
 				msg.time = toIntFromString(token[i+1]);
 
 			else if (token[i].compare("euid") == 0)
-				msg.device.euid = toIntFromString(token[i+1]);
+				msg.device.euid = stoull(token[i+1], nullptr, 0);
 
 			else if (token[i].compare("device_id") == 0)
 				msg.device.device_id = toIntFromString(token[i+1]);

PanInterface.cpp
@@ -136,7 +136,7 @@ void PanInterface::setActuators(Command cmd) {
 	sendCmd(msg);
 }
 
-void PanInterface::deleteDevice(long long int id) {
+void PanInterface::deleteDevice(euid_t id) {
 	log.error("Delete device called with sensor_id = " + to_string(id) + "| This is not implemented yet!");
 }
 

PanInterface.h
@@ -60,11 +60,11 @@ class PanInterface {
 public:
 	PanInterface(IOTMessage _msg, std::shared_ptr<Aggregator> _agg);
 	void sendCmd(std::vector<uint8_t>);
-	void deleteDevice(long long int);
+	void deleteDevice(euid_t);
 	void setPairingMode();
 	void setActuators(Command cmd);
 	void sendCommandToPAN(Command cmd);
-	
+
 	int convertBattery(float bat_actual);
 	void msgFromPAN(uint8_t msg_type, std::vector<uint8_t> data);
 

PressureSensor.cpp
@@ -86,7 +86,7 @@ void PressureSensor::run(){
  * @param sensor_id
  * @return true if they are equal
  */
-bool PressureSensor::isPressureSensor(long long int sensor_id) {
+bool PressureSensor::isPressureSensor(euid_t sensor_id) {
 	return (sensor.euid == sensor_id);
 }
 

PressureSensor.h
@@ -48,7 +48,7 @@ class PressureSensor : public Poco::Runnable {
 	public:
 		PressureSensor(IOTMessage _msg, std::shared_ptr<Aggregator> _agg);
 		virtual void run();
-		bool isPressureSensor(long long int sensor_id);
+		bool isPressureSensor(euid_t sensor_id);
 		void parseCmdFromServer(Command cmd);
 
 };

VPT.h
@@ -52,7 +52,7 @@ public:
 	virtual void run();
 
 	void detectDevices();
-	bool isVPTSensor(long long int sensor_id);
+	bool isVPTSensor(euid_t sensor_id);
 	void parseCmdFromServer(Command cmd);
 
 private:
@@ -61,17 +61,17 @@ private:
 	std::unique_ptr<JSONDevices> json;
 	Poco::Logger& log;
 	IOTMessage msg;
-	std::map<long long int, VPTDevice> map_devices;
+	std::map<euid_t, VPTDevice> map_devices;
 	std::string password;
 	TT_Table tt;
 
 	bool createMsg(VPTDevice &device);
 	std::string buildPasswordHash(std::string content);
 	unsigned int nextWakeup(void);
-	void fetchAndSendMessage(std::map<long long int, VPTDevice>::iterator &device);
+	void fetchAndSendMessage(std::map<euid_t, VPTDevice>::iterator &device);
 	void pairDevices();
-	long long int parseDeviceId(std::string &content);
-	void updateDeviceWakeUp(long long int euid, unsigned int time);
+	euid_t parseDeviceId(std::string &content);
+	void updateDeviceWakeUp(euid_t euid, unsigned int time);
 	void processCmdSet(Command cmd);
 	void processCmdListen(void);
 };

VirtualSensorModule.cpp
@@ -114,7 +114,7 @@ void VirtualSensorModule::pairNewVS() {
 	}
 }
 
-bool VirtualSensorModule::isVirtualDevice(long long int sensor_id) {
+bool VirtualSensorModule::isVirtualDevice(euid_t sensor_id) {
 	for (VirtualSensor vs : sensors) {
 		if (vs.sensor.euid == sensor_id)
 			return true;

commit d5bf7cdcb01e3a4afc62574b8e2d6f51b192cdab

vpt: fix EUID computation for VPT
The orignal function (from git:b1973f4) was wrong with extracting
the sub-MAC for EUID. The substr accepts offset + _length_ (and not
start + end). The goal is to extract the lowest 6 chars from the ID
string in format "XXXXXX######" ('#' is a hexa-decimal char we want
to extract).

After euid_t extraction (from git:a452471), the conversion is
broken even more by adding auto-detection of the number format -
stoull(..., 16) -> stoull(..., 0). Now, we force the hexa decimal
format again.

Fixes: a4524712c6b5 ("euid: extract data type euid_t")
Fixes: b1973f45eee4 ("vpt: Add new universal module for...")

Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

VPT.cpp
@@ -31,6 +31,11 @@ using Poco::Util::IniFileConfiguration;
 #define VPT_ID_MASK   0x00ffffff
 #define SEND_RETRY 3
 
+#define HEX_NUMBER 16
+
+#define SUB_MAC_START 6
+#define SUB_MAC_LENGTH 6
+
 const string ACTION_PAIR = "P";
 const string ACTION_READ = "R";
 const string ACTION_SET = "S";
@@ -223,7 +228,7 @@ string VPTSensor::buildPasswordHash(std::string content) {
 euid_t VPTSensor::parseDeviceId(string &content)
 {
 	string id = json->getParameterValuesFromContent("id", content);
-	euid_t euid = stoull(id.substr(5,11), nullptr, 0);
+	euid_t euid = stoull(id.substr(SUB_MAC_START, SUB_MAC_LENGTH), nullptr, HEX_NUMBER);
 	return VPT_ID_PREFIX | (euid & VPT_ID_MASK);
 }
 