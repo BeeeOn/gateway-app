BeeeOn/gateway-app

commit 41538ed9078422229a20004186f05f0ff888b76b

vpt: remove device, if not found specification of device

VPT.cpp
@@ -346,6 +346,7 @@ void VPTSensor::pairDevices(void) {
 			json->loadDeviceConfiguration(device->second.name);
 		}
 		catch (Poco::Exception &e) {
+			map_devices.erase(device);
 			log.error("VPT: " + e.displayText());
 		}
 	}

commit 569da0e074e0d8a050f004c4053aebb3be4ff896

vpt: fix delete devices.
Direct delete device from map of devices, can change end pointer.
It can do one iteration extra, which end as Seg. Fault.

Fixes: 41538ed90784 ("vpt: remove device, if not found specification of device")

Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

VPT.cpp
@@ -425,14 +425,19 @@ void VPTSensor::convertPressure(vector<Value> &values) {
 }
 
 void VPTSensor::pairDevices(void) {
+	vector<map<euid_t, VPTDevice>::iterator> devices_del;
+
 	for(auto device = map_devices.begin(); device != map_devices.end(); device++) {
 		try {
 			json->loadDeviceConfiguration(device->second.name, device->second.page_version);
 			updateTimestampOnVPT(device->second, ACTION_PAIR);
 		}
 		catch (Poco::Exception &e) {
-			map_devices.erase(device);
+			devices_del.push_back(device);
 			log.error("VPT: " + e.displayText());
 		}
 	}
+
+	for(auto it = devices_del.begin(); it != devices_del.end(); it++)
+		map_devices.erase(*it);
 }