BeeeOn/gateway-app

commit 4994af80fe5dea2e2987c6f3f454853aa74e0f4e

aggregator: remove ownership cycles; move initialization of Parameters
Self reference causes that object creation had to be split. Aggregator
object was semi initialized because of that split.

This whole change was done because of late initialization of Parameters
which caused a segfault.

Signed-off-by: Tomas Novotny <tomas@novotny.cz>
Signed-off-by: Adam Trhon <adam.trhon@gmail.com>

main.cpp
@@ -211,15 +211,12 @@ int main (int, char**) {
 
 		/* Mandatory module for sending and receiving data */
 		shared_ptr<Aggregator> agg (new Aggregator(msg, mosq));
-		agg->setAgg(agg);
-		agg_thread.start(*agg.get());
 
 		msg.state = "register";
 		msg.time = time(NULL);
 		/* Mandatory component for receiving asynchronous messages from server */
 		shared_ptr<IOTReceiver> receiver (new IOTReceiver(agg, IP_addr_out, port_out, msg, adapter_id));
 		receiver->keepaliveInit(cfg);
-		srv_thread.start(*receiver.get());
 		agg->setTCP(receiver);
 
 		// BeeeOn PAN coordinator module

Aggregator.cpp
@@ -89,6 +89,7 @@ Aggregator::Aggregator(IOTMessage _msg, shared_ptr<MosqClient> _mq) :
 	LEDControl::blinkLED(LED_PAN, 3);
 #endif
 
+	param = shared_ptr<Parameters> (new Parameters(*this, msg_default));
 }
 
 void Aggregator::sendToPANviaMQTT(std::vector<uint8_t> msg) {
@@ -124,7 +125,7 @@ void Aggregator::run() {
 
 		// Create distributor
 		if (cfg->getBool("distributor.enabled", false))
-			dist.reset(new Distributor(agg, mq));
+			dist.reset(new Distributor(*this, mq));
 	}
 	catch (Poco::Exception& ex) {
 		log.error("Exception with config file reading:\n" + ex.displayText());
@@ -133,8 +134,6 @@ void Aggregator::run() {
 
 	setLoggingLevel(log, cfg); /* Set logging level from configuration file*/
 	setLoggingChannel(log, cfg); /* Set where to log (console, file, etc.)*/
-
-	param = shared_ptr<Parameters> (new Parameters(agg, msg_default));
 
 	Poco::Thread distThread("Distributor thread");
 	if (dist)
@@ -227,7 +226,6 @@ void Aggregator::run() {
 	}
 
 	distThread.join();
-	agg.reset(); /* destroy self reference */
 }
 
 pair<bool, Command> Aggregator::sendData(IOTMessage _msg) {
@@ -259,10 +257,6 @@ pair<bool, Command> Aggregator::sendData(IOTMessage _msg) {
 		dist->addNewMessage(msg); // Secondary function to add message to the history
 
 	return retval;
-}
-
-void Aggregator::setAgg(shared_ptr<Aggregator> _agg) {
-	agg = _agg;
 }
 
 void Aggregator::setPAN(shared_ptr<PanInterface> _pan) {

Aggregator.h
@@ -143,7 +143,6 @@ private:
 	Poco::Logger& log;
 	std::unique_ptr<Distributor> dist;
 
-	std::shared_ptr<Aggregator> agg; /* needs to hold self reference */
 	std::shared_ptr<PressureSensor> psm;
 	std::shared_ptr<VirtualSensorModule> vsm;
 	std::shared_ptr<PanInterface> pan;

Distributor.cpp
@@ -19,7 +19,7 @@ void Distributor::run() {
 	log.information("Distributor Thread running.");
 
 	if (mq.get() != nullptr){
-		mq->setAgg(agg);
+		mq->setAgg(&agg);
 	}
 
 	while(!quit_global_flag) {
@@ -54,7 +54,7 @@ void Distributor::run() {
 	}
 }
 
-Distributor::Distributor(shared_ptr<Aggregator> _agg, shared_ptr<MosqClient> _mq) :
+Distributor::Distributor(Aggregator &_agg, shared_ptr<MosqClient> _mq) :
 	geek_mode_enabled(false),
 	agg(_agg),
 	mq(_mq),

Distributor.h
@@ -40,12 +40,12 @@ class Distributor: public Poco::Runnable {
 		std::deque<IOTMessage> pending_msgs;                  // vector of unsent messages
 		std::unique_ptr<Poco::FastMutex> history_lock;          // Lock for inserting or reading values in history vector
 		std::unique_ptr<Poco::FastMutex> pending_lock;          // Lock for inserting or reading values in pending messages vector
-		std::shared_ptr<Aggregator> agg;                  // pointer to aggregator for calling "need more history data" messages
+		Aggregator &agg;                  // pointer to aggregator for calling "need more history data" messages
 		std::shared_ptr<MosqClient> mq;                   // pointer to Mosquitto client for MQTT communication
 		Poco::Logger& log;
 
 	public:
-		Distributor(std::shared_ptr<Aggregator> _agg, std::shared_ptr<MosqClient> _mq);
+		Distributor(Aggregator &_agg, std::shared_ptr<MosqClient> _mq);
 		virtual void run();
 		~Distributor();
 

MosqClient.cpp
@@ -22,6 +22,7 @@ MosqClient::MosqClient(std::string client_id_, std::string main_topic_, std::str
 	host(host_),
 	port(port_),
 	log(Poco::Logger::get("Adaapp-MQTT")),
+	agg(nullptr),
 	connected(false)
 {
 	log.setLevel("trace"); // set default lowest level
@@ -52,7 +53,7 @@ bool MosqClient::send_message(std::string message, std::string topic_, int qos) 
 	return ((publish(NULL, topic_.c_str(), message.length(), message.c_str(), qos)) == MOSQ_ERR_SUCCESS );
 }
 
-void MosqClient::setAgg(shared_ptr<Aggregator> agg_) {
+void MosqClient::setAgg(Aggregator* agg_) {
 	agg = agg_;
 }
 
@@ -78,7 +79,7 @@ void MosqClient::newMessageFromPAN(std::string msg) {
 	//uint8_t last = toIntFromString(s.substr(pos + delimiter.length()));
 	//vec.push_back(last);
 
-	if (agg.get())
+	if (agg)
 		agg->sendFromPAN(type, vec);
 }
 
@@ -156,7 +157,7 @@ bool MosqClient::add_topic_to_subscribe (std::string topic_) {
 
 void MosqClient::newMsgFromHAB(std::string msg_text) {
 
-	if(agg.get()){
+	if(agg){
 		agg->sendHABtoServer(msg_text);
 	}
 	else {
@@ -166,7 +167,7 @@ void MosqClient::newMsgFromHAB(std::string msg_text) {
 
 void MosqClient::askTheServer(string msg_text){
 	log.information("Mqtt ask the server for parameter " + msg_text);
-	if(agg.get()){
+	if(agg){
 		CmdParam param;
 		param.param_id = toIntFromString(msg_text);
 		param = agg->sendParam(param);

MosqClient.h
@@ -30,7 +30,7 @@ private:
 	std::string host;
 	int port;
 	Poco::Logger& log;
-	std::shared_ptr<Aggregator> agg;
+	Aggregator* agg;
 
 public:
 	bool connected;
@@ -49,7 +49,7 @@ public:
 	MosqClient(std::string client_id_, std::string main_topic_, std::string msg_prefix, std::string host_ = "localhost", int port_=1883);
 	~MosqClient();
 
-	void setAgg(std::shared_ptr<Aggregator> agg_);
+	void setAgg(Aggregator* agg_);
 	void newMessageFromPAN(std::string msg);
 
 	/**

Parameters.cpp
@@ -14,7 +14,7 @@ using Poco::Util::IniFileConfiguration;
 using Poco::Net::NetworkInterface;
 using Poco::Net::IPAddress;
 
-Parameters::Parameters(shared_ptr<Aggregator> _agg, IOTMessage _msg) :
+Parameters::Parameters(Aggregator &_agg, IOTMessage _msg) :
 	log(Poco::Logger::get("Adaapp-Param")),
 	agg(_agg),
 	msg(_msg)
@@ -64,7 +64,7 @@ bool Parameters::cmdFromServer(Command cmd)
 		msg.params = filled_params;	// add parameters block (struct)
 		msg.state = "parameters";	// for answer is this state
 		msg.time = time(NULL);		// set actual time
-		agg->sendData(msg);			// send (response) to server
+		agg.sendData(msg);			// send (response) to server
 		return true;
 	}
 	else if (cmd.state == "parameters") { // response from the server or other information
@@ -102,7 +102,7 @@ CmdParam Parameters::askServer(CmdParam cmd_request)
 	msg.state = "getparameters";	// for request is this state
 	msg.time = time(NULL);			// set actual time
 	log.information("Ask the Server | state = getparameters | param_id = " + toStringFromInt(cmd_request.param_id));
-	pair<bool, Command> response = agg->sendData(msg);	// send to server
+	pair<bool, Command> response = agg.sendData(msg);	// send to server
 	if (response.first && response.second.state == "parameters"){
 		log.information("Ask the Server | return OK");
 		justPrintToLog(response.second.params);

commit 3ac8d462d5c7092fc10b50ec22b57127a331c8bb

main: split creating and starting of the threads
Lets do the full initialization of the modules first. Start them after
the initialization. This behaviour is better, because modules will be
fully initialized before starting the threads.

Some memory leaks are fixed as a side effect of this change. These
memory leaks were introduced in the previous commit (4994af80).

Signed-off-by: Tomas Novotny <tomas@novotny.cz>
Signed-off-by: Adam Trhon <adam.trhon@gmail.com>

main.cpp
@@ -221,38 +221,57 @@ int main (int, char**) {
 
 		// BeeeOn PAN coordinator module
 		if (mod_pan) {
-			log.information("Starting PAN module.");
+			log.information("Creating PAN module.");
 			pan.reset(new PanInterface(msg, agg));
 			pan->set_pan(pan); // XXX Function name should be same
 			agg->setPAN(pan);
 		}
 
 		if (mod_vpt) {
-			log.information("Starting VPT module.");
+			log.information("Creating VPT module.");
 			vptsensor.reset(new VPTSensor(msg, agg, adapter_id));
-			vpt_thread.start(*vptsensor.get());
 			agg->setVPT(vptsensor);
 		}
 
 		if (mod_openhab && mod_mqtt) {
-			log.information("Starting OpenHAB module.");
+			log.information("Creating OpenHAB module.");
 			hab.reset(new OpenHAB(msg, agg));
-			hab_thread.start(*hab.get());
 			agg->setHAB(hab);
 		}
 
 		if (mod_pressure_sensor) {
-			log.information("Starting PressureSensors module.");
+			log.information("Creating PressureSensors module.");
 			psm.reset(new PressureSensor(msg, agg));
-			psm_thread.start(*psm.get());
 			agg->setPSM(psm);
 		}
 
 		if (mod_virtual_sensor) {
-			log.information("Starting VirtualSensors module.");
+			log.information("Creating VirtualSensors module.");
 			vsm.reset(new VirtualSensorModule(msg, agg));
-			vsm_thread.start(*vsm.get());
 			agg->setVSM(vsm);
+		}
+
+		agg_thread.start(*agg.get());
+		srv_thread.start(*receiver.get());
+
+		if (mod_vpt) {
+			log.information("Starting VPT module.");
+			vpt_thread.start(*vptsensor.get());
+		}
+
+		if (mod_openhab && mod_mqtt) {
+			log.information("Starting OpenHAB module.");
+			hab_thread.start(*hab.get());
+		}
+
+		if (mod_pressure_sensor) {
+			log.information("Starting PressureSensors module.");
+			psm_thread.start(*psm.get());
+		}
+
+		if (mod_virtual_sensor) {
+			log.information("Starting VirtualSensors module.");
+			vsm_thread.start(*vsm.get());
 		}
 
 		/* "Endless" loop waiting for SIGINT/SIGTERM */