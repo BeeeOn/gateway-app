BeeeOn/gateway-app

commit f8859b923c814d4355ab3b3a0477c42ac9b81fa9

protocol: add support for version 1.1, add messages getparameter/parameter
[tomas@novotny.cz: minor style fixes]

XMLTool.cpp
@@ -44,7 +44,7 @@ XMLTool::XMLTool(IOTMessage _msg) :
 
 /**
  * Create message which can be sent to server.
- * @param type Message type (0 - A_TO_S, 1 - INIT)
+ * @param type Message type (0 - A_TO_S, 1 - INIT, 2 - PARAM)
  * @return Created message in string
  */
 string XMLTool::createXML(int type) {
@@ -74,6 +74,9 @@ string XMLTool::createXML(int type) {
 		else if (type == INIT) {
 			writer.characters(" ");
 		}
+		else if (type == PARAM) {
+			createParam(&writer, msg.params, msg.state);
+		}
 
 		writer.endElement("", "adapter_server", "");
 		writer.endDocument();
@@ -96,6 +99,9 @@ void XMLTool::createDevice(XMLWriter* w, Device dev, bool debug, string proto, s
 	AttributesImpl att;
 	att.addAttribute("", "", "euid", "", toStringFromLongHex(dev.euid));
 	att.addAttribute("", "", "device_id", "", toStringFromLongHex(dev.device_id, 2));
+	if(dev.name != ""){
+		att.addAttribute("", "", "name", "", dev.name);
+	}
 
 	w->startElement("", "device", "", att);
 
@@ -122,6 +128,22 @@ void XMLTool::createDevice(XMLWriter* w, Device dev, bool debug, string proto, s
 		w->endElement("", "values", "");
 	}
 	w->endElement("", "device", "");
+}
+
+void XMLTool::createParam(XMLWriter* w, CmdParam par, string state){
+	AttributesImpl att;
+	att.addAttribute("", "", "param_id", "", toStringFromInt(par.param_id));
+	if (par.euid > 0)
+		att.addAttribute("", "", "euid", "", toStringFromLongHex(par.euid));
+
+	w->startElement("", "parameter", "", att);
+
+	if (state == "parameters" && par.value.size()) {
+		for (auto item: par.value){
+			w->dataElement("", "", "value", item.first);
+		}
+	}
+	w->endElement("", "parameter", "");
 }
 
 /**
@@ -175,18 +197,52 @@ Command XMLTool::parseXML(string str) {
 			}
 
 			else if (pNode->nodeName().compare("value") == 0) {
-				float val = atof(pNode->innerText().c_str());
+				if(cmd.state == "getparameters" || cmd.state == "parameters"){
+					string inner = pNode->innerText();
+					string device_id = "";
 
+					if (pNode->hasAttributes()) {
+						attributes = pNode->attributes();
+						string device_id = "";
+						for(unsigned int i = 0; i < attributes->length(); i++) {
+							attribute = attributes->item(i);
+							if (attribute->nodeName().compare("device_id") == 0) {
+								device_id = toNumFromString(attribute->nodeValue());
+							}
+						}
+						attributes->release();
+					}
+					cmd.params.value.push_back({inner, device_id});
+				}
+				else {
+					float val = atof(pNode->innerText().c_str());
+
+					if (pNode->hasAttributes()) {
+						int module_id = 0;
+						attributes = pNode->attributes();
+						for(unsigned int i = 0; i < attributes->length(); i++) {
+							attribute = attributes->item(i);
+							if (attribute->nodeName().compare("module_id") == 0) {
+								module_id = toNumFromString(attribute->nodeValue());
+							}
+						}
+						cmd.values.push_back({module_id, val});  //TODO Hex number is processed wrongly
+						attributes->release();
+					}
+				}
+			}
+			else if (pNode->nodeName().compare("parameter") == 0) {
 				if (pNode->hasAttributes()) {
-					int module_id = 0;
 					attributes = pNode->attributes();
 					for(unsigned int i = 0; i < attributes->length(); i++) {
 						attribute = attributes->item(i);
-						if (attribute->nodeName().compare("module_id") == 0) {
-							module_id = toNumFromString(attribute->nodeValue());
+						if (attribute->nodeName().compare("param_id") == 0 || attribute->nodeName().compare("id") == 0) {
+							cmd.params.param_id = toNumFromString(attribute->nodeValue());
+						}
+						else if (attribute->nodeName().compare("euid") == 0) {
+							cmd.params.euid = toNumFromString(attribute->nodeValue());
 						}
 					}
-					cmd.values.push_back({module_id, val});  //TODO Hex number is processed wrongly
 					attributes->release();
 				}
 			}

Aggregator.cpp
@@ -64,10 +64,10 @@ void TimeWatchdog::run() {
 	active = false;
 }
 
-Aggregator::Aggregator(long long int _adapter_id, shared_ptr<MosqClient> _mq) :
+Aggregator::Aggregator(IOTMessage _msg, shared_ptr<MosqClient> _mq) :
 	log(Poco::Logger::get("Adaapp-AGG")),
 	cache_last_storing_time(time(NULL)),
-	adapter_id(_adapter_id),
+	msg_default(_msg),
 	watchdog(std::chrono::steady_clock::now()),
 	mq(_mq)
 {
@@ -134,11 +134,13 @@ void Aggregator::run() {
 	setLoggingLevel(log, cfg); /* Set logging level from configuration file*/
 	setLoggingChannel(log, cfg); /* Set where to log (console, file, etc.)*/
 
+	param = shared_ptr<Parameters> (new Parameters(agg, msg_default));
+
 	Poco::Thread distThread("Distributor thread");
 	if (dist)
 		distThread.start(*dist);
 
-	std::unique_ptr<OpenHAB> haba(new OpenHAB(iotmsg));
+	std::unique_ptr<OpenHAB> haba(new OpenHAB(msg_default));
 	hab = std::move(haba);
 
 	// Button's handler thread
@@ -237,15 +239,15 @@ pair<bool, Command> Aggregator::sendData(IOTMessage _msg) {
 	pair<bool, Command> retval = std::pair<bool, Command>(false, c);
 
 	// Send valid message
-	if (isTimeValid(msg.time))
+	if (isTimeValid(msg.time)){
 		retval = tcp->sendToServer(msg);
-	else {
+	} else {
 		// Message with invalid timestamp came in valid time
 		msg.valid = false;
 		msg.offset = watchdog.getOffset();
 	}
 
-	if (!retval.first) {
+	if (!retval.first && msg.state == "data") {
 		if (isTimeValid(msg.time))
 			log.warning("Failed to send message (ts=" + to_string(msg.time) + ") to server - save to cache!");
 		else
@@ -286,7 +288,12 @@ void Aggregator::setTCP(shared_ptr<IOTReceiver> _tcp) {
 	tcp = _tcp;
 }
 
+void Aggregator::setHAB(shared_ptr<OpenHAB> _hab) {
+	hab = _hab;
+}
+
 void Aggregator::parseCmd(Command cmd) {
+	log.information("Agg: parseCmd, Command = " + cmd.state);
 
 	if (cmd.state == "listen") {
 		if (vsm && vsm->unpairedSensorsLeft()) {
@@ -300,6 +307,13 @@ void Aggregator::parseCmd(Command cmd) {
 		if (vpt) {
 			vpt->parseCmdFromServer(cmd);
 		}
+		if (hab) {
+			hab->cmdFromServer(cmd);
+		}
+	}
+	else if (cmd.state == "getparameters" || cmd.state == "parameters"){
+		log.information("Incoming GET-PARAMETER command");
+		param->cmdFromServer(cmd);
 	}
 	else {
 		if (psm && psm->isPressureSensor(cmd.euid)) {
@@ -314,12 +328,9 @@ void Aggregator::parseCmd(Command cmd) {
 			log.information("Sending incoming command to VPT");
 			vpt->parseCmdFromServer(cmd);
 		}
-		else if (hab && hab->isOpenhabDevice(cmd.device_id)) {
+		else if (hab) {
 			log.information("Sending incoming command to HAB");
-			std::vector<std::string> vales = hab->cmdFromServer(cmd);
-			for(auto str: vales){
-				mq->send_message(str,("BeeeOn/openhab/" + toStringFromLongInt(cmd.euid)),0);
-			}
+			hab->cmdFromServer(cmd);
 		}
 		else if (pan.get() != nullptr) {
 			log.information("Sending incoming command to PAN");
@@ -446,7 +457,7 @@ void Aggregator::loadCache() {
 			msg.valid = false;
 			log.information("This message has been stored with invalid TS, but now the time is not valid neither - so i can count offset from msg TS and actual TS.");
 		}
-		msg.adapter_id = toStringFromLongHex(adapter_id);
+		msg.adapter_id = msg_default.adapter_id;
 		cache.insert(std::pair<Cache_Key, IOTMessage>(Cache_Key(msg.priority, msg.time), msg));
 	}
 	log.information("Loading of persistent cache is complete. Cache contains " + std::to_string(cache.size()) + " items.");
@@ -557,12 +568,16 @@ float Aggregator::convertValue(TT_Module type, float old_val, bool reverse) {
 	return new_val;
 }
 
-void Aggregator::setIOTmsg(IOTMessage msg){
-	iotmsg = msg;
+void Aggregator::sendHABtoServer(std::string msg_text){
+	if (hab) {
+		IOTMessage msg = hab->msgFromMqtt(msg_text);
+		if(msg.time != 0)
+			sendData(msg);
+	} else {
+		log.information("New message on MQTT from OpenHAB, but module in NOT enabled.");
+	}
 }
 
-void Aggregator::sendHABtoServer(std::string msg_text){
-	IOTMessage msg = hab->msgFromMqtt(msg_text);
-	if(msg.time != 0)
-		sendData(msg);
+CmdParam Aggregator::sendParam(CmdParam par){
+	return param->askServer(par);
 }

Aggregator.h
@@ -30,6 +30,7 @@ extern bool quit_global_flag;
 #include "Distributor.h"
 #include "MosqClient.h"
 #include "Openhab.h"
+#include "Parameters.h"
 #include "PressureSensor.h"
 #include "utils.h"
 #include "VirtualSensorModule.h"
@@ -78,6 +79,7 @@ class PanInterface;
 class VirtualSensorModule;
 class Distributor;
 class IOTReceiver;
+class Parameters;
 
 /**
  * Class to watch invalid time. If there is no valid time, messages are not sent to the server and offset to blackout is computed. Correct time is computed after restoration of time.
@@ -109,7 +111,7 @@ public:
  */
 class Aggregator: public Poco::Runnable {
 public:
-	Aggregator(long long int _adapter_id, std::shared_ptr<MosqClient> _mq);
+	Aggregator(IOTMessage _msg, std::shared_ptr<MosqClient> _mq);
 	void run();
 	std::pair<bool, Command> sendData(IOTMessage _msg);
 	virtual ~Aggregator();
@@ -118,6 +120,7 @@ public:
 	void setPSM(std::shared_ptr<PressureSensor> _psm);
 	void setPAN(std::shared_ptr<PanInterface> _pan);
 	void setVPT(std::shared_ptr<VPTSensor> _vpt);
+	void setHAB(std::shared_ptr<OpenHAB> _hab);
 	void setTCP(std::shared_ptr<IOTReceiver> _tcp);
 	void storeCache();
 	void loadCache(void);
@@ -130,8 +133,8 @@ public:
 	float convertValue(TT_Module type, float old_val, bool reverse=false);
 
 	void setAgg(std::shared_ptr<Aggregator> _agg);
-	void setIOTmsg(IOTMessage msg);
 	void sendHABtoServer(std::string msg_text);
+	CmdParam sendParam(CmdParam param);
 
 private:
 	std::unique_ptr<Poco::FastMutex> cache_lock;
@@ -144,7 +147,9 @@ private:
 	std::shared_ptr<VirtualSensorModule> vsm;
 	std::shared_ptr<PanInterface> pan;
 	std::shared_ptr<VPTSensor> vpt;
+	std::shared_ptr<OpenHAB> hab;
 	std::shared_ptr<IOTReceiver> tcp;
+	std::shared_ptr<Parameters> param;
 
 	std::thread button_t;
 
@@ -153,14 +158,11 @@ private:
 	long long int cache_last_storing_time;
 	std::string permanent_cache_path;
 
-	long long int adapter_id;
+	IOTMessage msg_default;
 	TimeWatchdog watchdog;
 	std::shared_ptr<MosqClient> mq;
 
 	void printCache(bool verbose);
-
-	IOTMessage iotmsg;
-	std::unique_ptr<OpenHAB> hab;
 
 };
 

CMakeLists.txt
@@ -62,7 +62,7 @@ check_cxx_function_exists(std::stoul ${CXX_FUNCTION_FAMILY_TO_NUMBER} HAVE_CXX_S
 check_cxx_function_exists(std::stoull ${CXX_FUNCTION_FAMILY_TO_NUMBER} HAVE_CXX_STOULL)
 check_cxx_function_exists(std::stof ${CXX_FUNCTION_FAMILY_TO_NUMBER} HAVE_CXX_STOF)
 
-set (ADAAPP_SOURCES main.cpp XMLTool.cpp PanInterface.cpp TCP.cpp Aggregator.cpp VirtualSensorModule.cpp VirtualSensor.cpp VirtualSensorValue.cpp Distributor.cpp IOcontrol.cpp MosqClient.cpp PressureSensor.cpp Openhab.cpp VPT.cpp JSON.cpp HTTP.cpp)
+set (ADAAPP_SOURCES main.cpp XMLTool.cpp PanInterface.cpp TCP.cpp Aggregator.cpp VirtualSensorModule.cpp VirtualSensor.cpp VirtualSensorValue.cpp Distributor.cpp IOcontrol.cpp MosqClient.cpp PressureSensor.cpp Openhab.cpp VPT.cpp JSON.cpp HTTP.cpp Parameters.cpp)
 
 add_executable (${PROJECT_NAME} ${ADAAPP_SOURCES})
 

MosqClient.cpp
@@ -139,6 +139,9 @@ void MosqClient::on_message (const struct mosquitto_message *message) {
 	if (msg_topic.compare("BeeeOn/openhab") == 0) {
 		newMsgFromHAB(msg_text);
 	}
+	if (msg_topic.compare("BeeeOn/param") == 0) {
+		askTheServer(msg_text);
+	}
 }
 
 void MosqClient::on_subscribe (int mid, int qos_count, const int *granted_qos) {
@@ -153,13 +156,27 @@ bool MosqClient::add_topic_to_subscribe (std::string topic_) {
 
 void MosqClient::newMsgFromHAB(std::string msg_text) {
 
-	//std::unique_ptr<OpenHAB> hab(new OpenHAB(iotmsg, agg));
-	//hab->msgFromMqtt(msg);
-	// "The object is automatically deleted when the scope ends."
-
 	if(agg.get()){
 		agg->sendHABtoServer(msg_text);
+	}
+	else {
+		log.information("newMsgFromHAB | Missing agg");
+	}
+}
+
+void MosqClient::askTheServer(string msg_text){
+	log.information("Mqtt ask the server for parameter " + msg_text);
+	if(agg.get()){
+		CmdParam param;
+		param.param_id = toIntFromString(msg_text);
+		param = agg->sendParam(param);
+		if (param.value.size()){
+			for(auto item: param.value){
+				log.information("Server answer: " + item.first);
+			}
+			log.information("Server answers done.");
+		} else log.information("No values from server");
 	} else {
-		log.information("Missing agg");
+		log.error("askTheServer | Missing agg");
 	}
 }

MosqClient.h
@@ -77,6 +77,8 @@ public:
 	bool add_topic_to_subscribe(std::string topic);
 
 	void newMsgFromHAB(std::string msg_text);
+
+	void askTheServer(std::string msg_text);
 };
 
 #endif	/* MYMOSQ_H */

Parameters.cpp
@@ -1,0 +1,173 @@
+/**
+ * @file Parameters.cpp
+ * @Author BeeeOn team (MS)
+ * @date April, 2016
+ * @brief
+ */
+
+#include "Parameters.h"
+
+using namespace std;
+using Poco::AutoPtr;
+using Poco::Logger;
+using Poco::Util::IniFileConfiguration;
+using Poco::Net::NetworkInterface;
+using Poco::Net::IPAddress;
+
+Parameters::Parameters(shared_ptr<Aggregator> _agg, IOTMessage _msg) :
+	log(Poco::Logger::get("Adaapp-Param")),
+	agg(_agg),
+	msg(_msg)
+{
+	log.setLevel("trace"); // set default lowest level
+	ada_type = "other";
+
+	AutoPtr<IniFileConfiguration> cfg_param;
+	try {
+		cfg_param = new IniFileConfiguration(string(MODULES_DIR)+string(MOD_PARAM)+".ini");
+		setLoggingLevel(log, cfg_param); // Set logging level from config file
+		setLoggingChannel(log, cfg_param); // Set where to log ( console, file, etc.)
+		ada_type = cfg_param->getString(string(MOD_PARAM) + ".type", "other");
+	}
+	catch (Poco::Exception& ex) {
+		log.error("Error: Exception with config file reading: " + (string)ex.displayText());
+		ada_type = "other";
+	}
+
+	msg.state = "getparameters";
+	msg.priority = MSG_PRIO_PARAM;
+	msg.offset = 0;
+
+	log.information("=== Adaapp Started ===");
+}
+
+/**
+ * Processing the request from the server.
+ * Return true in case of success or false if a message is not supported.
+ */
+bool Parameters::cmdFromServer(Command cmd)
+{
+	if (toFloatFromString(cmd.protocol_version) < 1.1) {
+		return false;
+	}
+	if (cmd.state == "getparameters") { // new request from the server
+		log.information("cmdFromServer | state == getparameters");
+		CmdParam filled_params;		// (will be) filled struct for send
+		switch (cmd.params.param_id) {
+			case 2000: filled_params = getDummy(cmd.params); break;
+			case 2001: filled_params = getLocalIp(cmd.params); break;
+			case 2002: filled_params = getGatetype(cmd.params); break;
+			default: return false;
+		}
+		msg.params = filled_params;	// add parameters block (struct)
+		msg.state = "parameters";	// for answer is this state
+		msg.time = time(NULL);		// set actual time
+		agg->sendData(msg);			// send (response) to server
+		return true;
+	}
+	else if (cmd.state == "parameters") { // response from the server or other information
+		log.information("cmdFromServer | state == parameters");
+		switch (cmd.params.param_id) {
+			case 1000: log.information("Server PING me"); break;
+			case 1001: /*label*/ justPrintToLog(cmd.params); break;
+			case 1002: /*room*/ justPrintToLog(cmd.params); break;
+			case 1003: allsensors(cmd.params); break;
+			case 1004: /*vptpasswd*/ justPrintToLog(cmd.params); break;
+			default: return false;
+		}
+	}
+	return true;
+}
+
+/**
+ * Sending a request to the server.
+ * This function concurrently returns an answer from the server.
+ * In case of an error returns the original unchanged structure.
+ */
+CmdParam Parameters::askServer(CmdParam cmd_request)
+{
+	if (cmd_request.param_id < 1000 || cmd_request.param_id > 1999){
+		log.error("Ask the Server: Wrong param_id");
+		return cmd_request;	// id of requirement must be specified
+	}
+	msg.params = cmd_request;		// add parameters block (struct)
+	msg.state = "getparameters";	// for request is this state
+	msg.time = time(NULL);			// set actual time
+	log.information("Ask the Server | state = getparameters | param_id = " + toStringFromInt(cmd_request.param_id));
+	pair<bool, Command> response = agg->sendData(msg);	// send to server
+	if (response.first && response.second.state == "parameters"){
+		log.information("Ask the Server | return OK");
+		justPrintToLog(response.second.params);
+		return response.second.params;	// prayers were answered
+	}
+	log.information("Ask the Server | return FAIL");
+	return cmd_request;
+}
+
+/**
+ * Returns a vector (list) of IP address (active connections).
+ */
+vector<string> Parameters::getIP()
+{
+	vector<string> list_of_ip;
+	NetworkInterface::Map m = NetworkInterface::map(false, false);
+	for (NetworkInterface::Map::const_iterator it = m.begin(); it != m.end(); ++it) {
+		if ( ! it->second.isUp())
+			continue;
+
+		typedef NetworkInterface::AddressList List;
+		const List& ipList = it->second.addressList();
+
+		for (auto ip: ipList) {
+			std::string tmp = ip.get<NetworkInterface::IP_ADDRESS>().toString();
+			if ( (! ip.get<NetworkInterface::IP_ADDRESS>().isLoopback()) &&		// no localhost
+				(ip.get<NetworkInterface::IP_ADDRESS>().family() == Poco::Net::IPAddress::IPv4) &&	// only IPv4
+				(std::find(list_of_ip.begin(), list_of_ip.end(), tmp) == list_of_ip.end()) ) {	// not in list
+					list_of_ip.push_back(tmp);		// add
+			}
+		}
+	}
+	return list_of_ip;
+}
+
+void Parameters::allsensors(CmdParam incomer)
+{
+	log.information("Refresh euid list");
+	euides.clear();
+	for(auto item: incomer.value){
+		log.information("  " + item.first + " (" + item.second + ")");
+		euides.push_back(item);
+	}
+}
+
+void Parameters::justPrintToLog(CmdParam incomer)
+{
+	log.information("Parameters from server, param_id = ", incomer.param_id);
+	for(auto item: incomer.value){
+		log.information("  " + item.first + " (" + item.second + ")");
+	}
+}
+
+CmdParam Parameters::getDummy(CmdParam incomer)
+{
+	log.information("get Dummy");
+	incomer.value.push_back({"",""});
+	return incomer;
+}
+
+CmdParam Parameters::getLocalIp(CmdParam incomer)
+{
+	log.information("get Local IP");
+	vector<string> ip_list = getIP();
+	for (auto item: ip_list){
+		incomer.value.push_back({item,""});
+	}
+	return incomer;
+}
+
+CmdParam Parameters::getGatetype(CmdParam incomer)
+{
+	log.information("get Gate type");
+	incomer.value.push_back({ada_type,""});
+	return incomer;
+}

Parameters.h
@@ -1,0 +1,46 @@
+/**
+ * @file Parameters.h
+ * @Author BeeeOn team (MS)
+ * @date April, 2016
+ * @brief
+ */
+
+#ifndef PARAMETERS_H
+#define	PARAMETERS_H
+
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <Poco/AutoPtr.h>
+#include <Poco/Logger.h>
+#include <Poco/Net/NetworkInterface.h>
+#include <Poco/Util/IniFileConfiguration.h>
+
+#include "Aggregator.h"
+#include "utils.h"
+
+class Parameters {
+public:
+	Parameters(std::shared_ptr<Aggregator> _agg, IOTMessage _msg);
+	bool cmdFromServer(Command cmd);
+	CmdParam askServer(CmdParam request);
+
+private:
+	std::string ada_type;
+	Poco::Logger& log;
+	std::shared_ptr<Aggregator> agg;
+	IOTMessage msg;
+	std::vector<std::pair<std::string, std::string> > euides;
+
+	std::vector<std::string> getIP();
+	void allsensors(CmdParam incomer);
+	void justPrintToLog(CmdParam incomer);
+	CmdParam getDummy(CmdParam incomer);
+	CmdParam getLocalIp(CmdParam incomer);
+	CmdParam getGatetype(CmdParam incomer);
+};
+
+#endif //PARAMETERS_H

commit be5bc90e34dc93e35c099177b31fb88df6c8d50d

xmltool: fix num parsing on getparameters
The param_id was parsed as a hexadecimal number instead of decimal.
The expected value is in decimal format as specified in get-params protocol.

Fixes: f8859b923c81 ("protocol: add support for version 1.1, add messages getparameter/parameter")

Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

XMLTool.cpp
@@ -245,7 +245,7 @@ Command XMLTool::parseXML(string str) {
 					for(unsigned int i = 0; i < attributes->length(); i++) {
 						attribute = attributes->item(i);
 						if (attribute->nodeName().compare("param_id") == 0 || attribute->nodeName().compare("id") == 0) {
-							cmd.params.param_id = toNumFromString(attribute->nodeValue());
+							cmd.params.param_id = toIntFromString(attribute->nodeValue());
 						}
 						else if (attribute->nodeName().compare("euid") == 0) {
 							cmd.params.euid = toNumFromString(attribute->nodeValue());