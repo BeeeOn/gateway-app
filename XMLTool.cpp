BeeeOn/gateway-app

commit 194e754ffdd86e62c5dfdc7e6ab5a27ff35e502b

logging: use LoggingConfigurator for Adaapp
Adaapp have been migrated to utilize Poco LoggingConfigurator for
logging configuration.

Logging configurations are now separated from general configurations.
Preserve logging into the original both file and console usign SplitterChannel.

New configuration file AdaAppLogging.ini has been created, which holds
logging configurations for all Adaapp modules.

These modules have migrated: main, Aggregator, Distributor, ModuleADT, PressureSensor,
MosqClient, VirtualSensor, Openhab, PanInterface, TCP, VPT, xmltool.

Signed-off-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Lukas Koszegy <l.koszegy@gmail.com>

XMLTool.cpp
@@ -20,7 +20,6 @@ XMLTool::XMLTool() :
 	msg(),
 	log(Poco::Logger::get("Adaapp-XML"))
 {
-	log.setLevel("trace"); // set default lowest level
 	AutoPtr<Poco::Util::IniFileConfiguration> cfg;
 	try {
 		cfg = new Poco::Util::IniFileConfiguration(std::string(MODULES_DIR)+std::string(MOD_XML)+".ini");
@@ -29,8 +28,6 @@ XMLTool::XMLTool() :
 		log.error("Exception with config file reading:\n" + ex.displayText());
 		exit (EXIT_FAILURE);
 	}
-	setLoggingLevel(log, cfg); /* Set logging level from configuration file*/
-	setLoggingChannel(log, cfg); /* Set where to log (console, file, etc.)*/
 }
 
 /**

utils.h
@@ -61,13 +61,10 @@
 #define LED_LIME "/sys/devices/leds/leds/a10-olinuxino-lime:green:usr/"
 #define LED_PAN  "/sys/devices/leds/leds/a10-olinuxino-lime:green:LED1_OL/"
 
-#ifndef RELATIVE_PATH
-  #define CONFIG_FILE "/etc/beeeon/AdaApp.ini"
-#else
-  #define CONFIG_FILE RELATIVE_PATH "/beeeon/AdaApp.ini"
-#endif
-
 // mainly for .ini files and logs
+#define CONFIG_FILE MODULES_DIR "AdaApp.ini"
+#define LOGGERS_CONFIG_FILE MODULES_DIR "AdaAppLogging.ini"
+
 #define MOD_PAN             (std::string)"pan"
 #define MOD_VIRTUAL_SENSOR  (std::string)"virtual_sensor"
 #define MOD_PRESSURE_SENSOR (std::string)"pressure_sensor"

Aggregator.cpp
@@ -74,7 +74,6 @@ Aggregator::Aggregator(IOTMessage _msg, shared_ptr<MosqClient> _mq) :
 	cache_lock.reset(new FastMutex);
 	cache.clear();
 
-	log.setLevel("trace"); // set default lowest level
 	AutoPtr<IniFileConfiguration> cfg;
 	try {
 		cfg = new IniFileConfiguration(CONFIG_FILE);
@@ -89,8 +88,6 @@ Aggregator::Aggregator(IOTMessage _msg, shared_ptr<MosqClient> _mq) :
 	catch (Poco::Exception& ex) {
 		log.error("Exception with config file reading:\n" + ex.displayText());
 	}
-	setLoggingLevel(log, cfg); // Set logging level from config file
-	setLoggingChannel(log, cfg); // Set where to log (console, file, etc.)
 
 #ifdef LEDS_ENABLED
 	LEDControl::blinkLED(LED_PAN, 3);

Distributor.cpp
@@ -60,8 +60,6 @@ Distributor::Distributor(Aggregator &_agg, shared_ptr<MosqClient> _mq) :
 	mq(_mq),
 	log(Poco::Logger::get("Adaapp-DIST"))
 {
-	log.setLevel("trace"); // set default lowest level
-
 	history_lock.reset(new FastMutex);
 	pending_lock.reset(new FastMutex);
 	pending_msgs.clear();
@@ -81,8 +79,6 @@ Distributor::Distributor(Aggregator &_agg, shared_ptr<MosqClient> _mq) :
 		log.error("Exception with config file reading:\n" + ex.displayText());
 	}
 
-	setLoggingLevel(log, cfg); /* Set logging level from configuration file*/
-	setLoggingChannel(log, cfg); /* Set where to log (console, file, etc.)*/
 }
 
 Distributor::~Distributor() {

ModuleADT.cpp
@@ -15,7 +15,6 @@ ModuleADT::ModuleADT(std::shared_ptr<Aggregator> agg_, std::string logger_name, 
 	using Poco::AutoPtr;
 	using Poco::Util::IniFileConfiguration;
 
-	log.setLevel("trace"); // set default lowest level
 	AutoPtr<IniFileConfiguration> cfg;
 
 	try {
@@ -26,8 +25,6 @@ ModuleADT::ModuleADT(std::shared_ptr<Aggregator> agg_, std::string logger_name, 
 		inicialized = false;
 		return;
 	}
-	setLoggingLevel(log, cfg); /* Set logging level from configuration file*/
-	setLoggingChannel(log, cfg); /* Set where to log (console, file, etc.)*/
 
 	sensor.version = 1;
 	sensor.pairs = 1;

MosqClient.cpp
@@ -25,8 +25,6 @@ MosqClient::MosqClient(std::string client_id_, std::string main_topic_, std::str
 	agg(nullptr),
 	connected(false)
 {
-	log.setLevel("trace"); // set default lowest level
-
 	AutoPtr<IniFileConfiguration> cfg;
 	try {
 		cfg = new IniFileConfiguration(std::string(MODULES_DIR)+std::string(MOD_MQTT)+".ini");
@@ -35,9 +33,6 @@ MosqClient::MosqClient(std::string client_id_, std::string main_topic_, std::str
 		log.error("Exception with config file reading:\n" + ex.displayText());
 		exit (EXIT_FAILURE);
 	}
-
-	setLoggingLevel(log, cfg); /* Set logging level from configuration file*/
-	setLoggingChannel(log, cfg); /* Set where to log (console, file, etc.)*/
 
 	mosqpp::lib_init();        // Mandatory initialization for mosquitto library
 	log.information("mosqClient \"" + id + "\" connecting to main topic \"" + topic + "\" to broker with interface \"" + host + ":" + std::to_string(port) + "\".");

Openhab.cpp
@@ -19,8 +19,6 @@ OpenHAB::OpenHAB(IOTMessage _msg, std::shared_ptr<Aggregator> _agg) :
 	log(Poco::Logger::get("Adaapp-HAB")),
 	msg(_msg)
 {
-	log.setLevel("trace"); // set default lowest level
-
 	AutoPtr<IniFileConfiguration> cfg;
 	try {
 		cfg = new IniFileConfiguration(string(MODULES_DIR)+string(MOD_OPENHAB)+".ini");
@@ -29,8 +27,6 @@ OpenHAB::OpenHAB(IOTMessage _msg, std::shared_ptr<Aggregator> _agg) :
 		log.error("OH: Exception with config file reading:\n" + ex.displayText());
 		exit (EXIT_FAILURE);
 	}
-	setLoggingLevel(log, cfg); /* Set logging level from configuration file*/
-	setLoggingChannel(log, cfg); /* Set where to log (console, file, etc.)*/
 
 	msg.state = "data";
 	msg.priority = MSG_PRIO_SENSOR;

PanInterface.cpp
@@ -21,7 +21,6 @@ PanInterface::PanInterface(IOTMessage _msg, shared_ptr<Aggregator> _agg) :
 	agg(_agg),
 	log(Poco::Logger::get("Adaapp-SPI"))
 {
-	log.setLevel("trace"); // set default lowest level
 	AutoPtr<IniFileConfiguration> cfg;
 	try {
 		cfg = new IniFileConfiguration(string(MODULES_DIR)+string(MOD_PAN)+".ini");
@@ -30,8 +29,6 @@ PanInterface::PanInterface(IOTMessage _msg, shared_ptr<Aggregator> _agg) :
 		log.fatal("Exception with config file reading:\n" + ex.displayText());
 		exit(EXIT_FAILURE);
 	}
-	setLoggingLevel(log, cfg); /* Set logging level from configuration file*/
-	setLoggingChannel(log, cfg); /* Set where to log (console, file, etc.)*/
 
 	msg.time = (long long int)time(0);
 

commit 7f4d74c2c818f408dd96c0594991cec7774fb969

xmltool: fix opening of file for not exist ini file
xmltool.ini was deleted in previous commit.

Fixes: 194e754 ("logging: use LoggingConfigurator for Adaapp")

Reported-by: Peter Tisovcik <xtisov00@stud.fit.vutbr.cz>
Signed-off-by: Lukas Koszegy <l.koszegy@gmail.com>
Tested-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>
Reviewed-by: Richard Wolfert <xwolfe00@stud.fit.vutbr.cz>

XMLTool.cpp
@@ -20,14 +20,6 @@ XMLTool::XMLTool() :
 	msg(),
 	log(Poco::Logger::get("Adaapp-XML"))
 {
-	AutoPtr<Poco::Util::IniFileConfiguration> cfg;
-	try {
-		cfg = new Poco::Util::IniFileConfiguration(std::string(MODULES_DIR)+std::string(MOD_XML)+".ini");
-	}
-	catch (Poco::Exception& ex) {
-		log.error("Exception with config file reading:\n" + ex.displayText());
-		exit (EXIT_FAILURE);
-	}
 }
 
 /**

utils.h
@@ -72,7 +72,6 @@
 #define MOD_MQTT            (std::string)"mqtt"
 #define MOD_OPENHAB         (std::string)"openhab"
 #define MOD_TCP         	(std::string)"tcp"
-#define MOD_XML         	(std::string)"xmltool"
 #define MOD_JSON         	(std::string)"json"
 #define MOD_PARAM			(std::string)"parameters"
 