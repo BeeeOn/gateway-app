BeeeOn/gateway-app

commit e31587ee98db58982be048129a1bd2204886755e

MQTTData: implement MQTTDataModule class
The class implements interface for module within the Adaapp. It uses mosquitto
for communication in JSON format. MQTTDataModule provides support for listening,
clearing and setting actuator messages from server and sending data to server
from sensor.

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

MQTTDataModule.cpp
@@ -1,0 +1,135 @@
+/**
+ * @file MQTTDataModule.cpp
+ * @Author BeeeOn team - Peter Tisovcik <xtisov00@stud.fit.vutbr.cz>
+ * @date September, 2016
+ */
+
+#include <ctime>
+
+#include <Poco/Dynamic/Var.h>
+#include <Poco/JSON/JSON.h>
+#include <Poco/JSON/Parser.h>
+
+#include "MQTTDataModule.h"
+#include "MQTTDataParser.h"
+#include "main.h"
+#include "utils.h"
+
+using namespace std;
+using Poco::AutoPtr;
+
+MQTTDataModule::MQTTDataModule(IOTMessage msg, shared_ptr<Aggregator> agg) :
+	ModuleADT(agg, "MQTTDataModule", MOD_MQTT_DATA, msg),
+	m_msg(msg)
+{
+}
+
+/**
+ * Main method of this thread.
+ *
+ * Naturally end when quit_global_flag is set to true,
+ * so the Adaapp is terminating.
+ */
+void MQTTDataModule::threadFunction()
+{
+	log.information("Starting MQTTDataModule thread...");
+
+	while(!quit_global_flag) {
+		sleep(1);
+	}
+}
+
+void MQTTDataModule::sendConfigCmd(std::string msg)
+{
+	agg->sendFromMQTTDataModule(msg, MQTT_DATA_MODULE_FROM_ADAAPP);
+}
+
+void MQTTDataModule::msgToMQTTDataModule(string msg_text)
+{
+	MQTTDataParser msgParser;
+	IOTMessage iot = msgParser.parseMessage(msg_text);
+	iot.time = time(NULL);
+	iot.adapter_id = m_msg.adapter_id;
+
+	if (!iot.valid) {
+		log.error("Invalid msg for MQTTDataModule", __FILE__, __LINE__);
+		return;
+	}
+
+	if (iot.state == "data")
+		agg->sendData(iot);
+	else
+		log.error("Unknown message state for MQTTDataModule", __FILE__, __LINE__);
+}
+
+void MQTTDataModule::parseCmdFromServer(const Command& cmd)
+{
+	if (cmd.state == "listen") {
+		log.debug("Send sensor pairing state command.");
+		sendConfigCmd(createPairingMessage());
+	}
+	else if (cmd.state == "clean") {
+		log.debug("Clean command to delete sensor.");
+		sendConfigCmd(createDeleteMessage(cmd.euid));
+	}
+	else if (cmd.state == "set") {
+		log.debug("Set actuator value(s) comand");
+		sendConfigCmd(createSetActuatorsMessage(cmd));
+	}
+	else if (cmd.state == "error") {
+		log.error("Error state in command -> Nothing to send via MQTT",
+			__FILE__, __LINE__);
+	}
+	else {
+		log.error("Unknown state of incomming command from server! Command state = "
+			+ cmd.state, __FILE__, __LINE__);
+	}
+}
+
+string MQTTDataModule::createPairingMessage() const
+{
+	Poco::JSON::Object jsonMsg;
+	jsonMsg.set("state", "listen");
+	Poco::Dynamic::Var jsonVar = jsonMsg;
+
+	return jsonVar.convert<std::string>();
+}
+
+string MQTTDataModule::createDeleteMessage(const euid_t& euid) const
+{
+	Poco::JSON::Object jsonMsg;
+	Poco::JSON::Object data;
+
+	data.set("euid", euid);
+	jsonMsg.set("device", data);
+	jsonMsg.set("state", "clean");
+	Poco::Dynamic::Var jsonVar = jsonMsg;
+
+	return jsonVar.convert<std::string>();
+}
+
+string MQTTDataModule::createSetActuatorsMessage(const Command& cmd) const
+{
+	Poco::JSON::Object jsonMsg;
+	Poco::JSON::Object sensorValues;
+	Poco::JSON::Object::Ptr data;
+	Poco::JSON::Array arrayPtr;
+
+	jsonMsg.set("state", "set");
+	jsonMsg.set("deviceID", std::to_string(cmd.device_id));
+	jsonMsg.set("euid", std::to_string(cmd.euid));
+
+	size_t i = 0;
+	for (auto item : cmd.values) {
+		data = new Poco::JSON::Object();
+
+		data->set("moduleID", to_string(item.first));
+		data->set("value", to_string(item.second));
+
+		arrayPtr.set(i++, data);
+		jsonMsg.set("data", arrayPtr);
+	}
+
+	Poco::Dynamic::Var jsonVar = jsonMsg;
+	return jsonVar.convert<std::string>();
+}

CMakeLists.txt
@@ -70,6 +70,7 @@ set (ADAAPP_SOURCES
 	IOcontrol.cpp
 	JSON.cpp HTTP.cpp
 	JablotronModule.cpp
+	MQTTDataModule.cpp
 	MQTTDataParser.cpp
 	ModuleADT.cpp
 	MosqClient.cpp

MQTTDataModule.h
@@ -1,0 +1,74 @@
+/**
+ * @file MQTTDataModule.h
+ * @Author BeeeOn team - Peter Tisovcik <xtisov00@stud.fit.vutbr.cz>
+ * @date September, 2016
+ */
+
+#ifndef MQTT_DATA_MODULE_H
+#define MQTT_DATA_MODULE_H
+
+#include <memory>
+#include <string>
+
+#include "MQTTDataParser.h"
+#include "ModuleADT.h"
+#include "device_table.h"
+#include "utils.h"
+
+#define MQTT_DATA_MODULE_TO_ADAAPP        (std::string)"BeeeOn/MQTTDataModule/ToAdaApp"
+#define MQTT_DATA_MODULE_FROM_ADAAPP      (std::string)"BeeeOn/MQTTDataModule/FromAdaApp"
+
+class Aggregator;
+
+/*
+ * The class implements interface for module within the Adaapp. It uses mosquitto
+ * for communication in JSON format. MQTTDataModule provides support for listening,
+ * clearing and setting actuator messages from server and sending data to server
+ * from sensor.
+ */
+class MQTTDataModule : public ModuleADT
+{
+	void threadFunction() override;
+
+public:
+	MQTTDataModule(IOTMessage msg, std::shared_ptr<Aggregator> agg);
+
+	void parseCmdFromServer(const Command & cmd) override;
+
+	/*
+	 * Send message from modul to MQTT
+	 * @param msg message
+	 */
+	void sendConfigCmd(std::string msg);
+
+	/*
+	 * Receive message from MQTT
+	 * @param received message
+	 */
+	void msgToMQTTDataModule(std::string msg);
+
+	/*
+	 * Create pairing message
+	 * {"state": "listen"}
+	 * @return Pairing message
+	 */
+	std::string createPairingMessage() const;
+
+	/*
+	 * Create delete message
+	 * {"device": { "euid": "" }, "state": "clean" }
+	 * @return Delete message
+	 */
+	std::string createDeleteMessage(const euid_t& euid) const;
+
+	/*
+	 * Create message which set actuator
+	 * {"state": "set", "euid": "", "data": [{"module_id": "", "value:": ""}]}
+	 * @return Set Message
+	 */
+	std::string createSetActuatorsMessage(const Command& cmd) const;
+
+private:
+	IOTMessage m_msg;
+};
+#endif

etc/AdaAppLogging.ini
@@ -86,6 +86,12 @@ loggers.l13.channel.channel1 = c1
 loggers.l13.channel.channel2 = c12
 loggers.l13.level = trace
 
+loggers.l14.name = MQTTDataModule
+loggers.l14.channel.class = SplitterChannel
+loggers.l14.channel.channel1 = c1
+loggers.l14.channel.channel2 = c13
+loggers.l14.level = trace
+
 ; CHANNELS
 channels.c1.class = ConsoleChannel
 channels.c1.formatter = f1
@@ -155,6 +161,12 @@ channels.c12.rotation = 1 M
 channels.c12.purgeCount = 1
 channels.c12.path = /tmp/jablotron.log
 channels.c12.formatter = f1
+
+channels.c13.class = FileChannel
+channels.c13.rotation = 1 M
+channels.c13.purgeCount = 1
+channels.c13.path = /tmp/mqtt_data_module.log
+channels.c13.formatter = f1
 
 ; FORMATTERS
 formatters.f1.class = PatternFormatter

etc/mqtt_data.ini
@@ -1,0 +1,2 @@
+[mqtt_data]
+enabled = true

utils.h
@@ -75,6 +75,7 @@
 #define MOD_JSON         	(std::string)"json"
 #define MOD_PARAM			(std::string)"parameters"
 #define MOD_JABLOTRON       (std::string)"jablotron"
+#define MOD_MQTT_DATA       (std::string)"mqtt_data"
 
 #define DEFAULT_LOG_FORMAT	(std::string)"[%Y-%m-%d %H:%M:%S, %q%q, %s, %T] %t"
 

commit 52e4d12beaabf9ea593e2157eaefc9b47b8ed9df

MQTTData: fix wrong device_id and module_id name
Fixes: e31587ee98db58982b ("MQTTData: implement MQTTDataModule class")
Fixes: 3a79330263ad2171d8 ("MQTTData: implement MQTTDataParser class")

Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

MQTTDataModule.cpp
@@ -116,14 +116,14 @@ string MQTTDataModule::createSetActuatorsMessage(const Command& cmd) const
 	Poco::JSON::Array arrayPtr;
 
 	jsonMsg.set("state", "set");
-	jsonMsg.set("deviceID", std::to_string(cmd.device_id));
+	jsonMsg.set("device_id", std::to_string(cmd.device_id));
 	jsonMsg.set("euid", std::to_string(cmd.euid));
 
 	size_t i = 0;
 	for (auto item : cmd.values) {
 		data = new Poco::JSON::Object();
 
-		data->set("moduleID", to_string(item.first));
+		data->set("module_id", to_string(item.first));
 		data->set("value", to_string(item.second));
 
 		arrayPtr.set(i++, data);

MQTTDataParser.cpp
@@ -29,7 +29,7 @@ IOTMessage MQTTDataParser::parseMessage(const string& data) const
 		parsedJson = parseJsonObject(data);
 
 		message.state = extractString(parsedJson, "state");
-		device.device_id = toIntFromString(extractString(parsedJson, "deviceID"));
+		device.device_id = toIntFromString(extractString(parsedJson, "device_id"));
 		device.euid = toIntFromString(extractString(parsedJson, "euid"));
 
 		Array::Ptr dataArray = parsedJson->getArray("data");
@@ -37,7 +37,7 @@ IOTMessage MQTTDataParser::parseMessage(const string& data) const
 		for (size_t i = 0; i < dataArray->size(); ++i) {
 			Object::Ptr item = dataArray->getObject(i);
 			string value = extractString(item, "value");
-			string moduleID = extractString(item, "moduleID");
+			string moduleID = extractString(item, "module_id");
 			device.values.push_back({signed(Poco::NumberParser::parseHex(moduleID)),
 				toFloatFromString(value)});
 		}