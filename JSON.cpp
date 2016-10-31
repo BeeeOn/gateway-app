BeeeOn/gateway-app

commit d376a713d73f54065b1ad6ee4d88d68f756106ad

vpt: add support for diffrent web pages
Now, the json.ini contains a key for each version of the web pages.
Device specification name in JSON module contains device name and version of web page

JSON.cpp
@@ -24,8 +24,12 @@ JSONDevices::JSONDevices(void) : log(Poco::Logger::get("Adaapp-VPT")) {
 	cfg = new Poco::Util::IniFileConfiguration(std::string(MODULES_DIR)+std::string(MOD_JSON)+".ini");
 }
 
-void JSONDevices::loadDeviceConfiguration(string device_name) {
-	string device_file = cfg->getString("devices."+device_name);
+void JSONDevices::loadDeviceConfiguration(string device_name, string page_version) {
+	if (page_version.empty())
+		page_version = "default";
+
+	string device_file = cfg->getString(device_name+"."+page_version);
+	device_name += "_" + page_version;
 	log.debug("JSON: Loading device specification for " + device_name);
 	Poco::DynamicStruct jsonStruct = loadFile(devices_folder+device_file);
 	checkBaseFormat(jsonStruct);
@@ -105,9 +109,10 @@ vector<Value> JSONDevices::getSensors(std::string content, std::string device_na
 	parser.reset();
 	log.debug("JSON: Get sensors values");
 	Poco::DynamicStruct jsonStruct = *(parser.parse(content).extract<Poco::JSON::Object::Ptr>());
-	if (device_name.empty()) {
-		device_name = jsonStruct["device"].toString();
-	}
+
+	if (device_name.empty())
+		device_name = jsonStruct["device"].toString() + "_" + jsonStruct["version"].toString();
+
 	if ((device_itr = devices.find(device_name)) == devices.end()) {
 		return values;
 	}

VPT.cpp
@@ -256,6 +256,8 @@ void VPTSensor::detectDevices(void) {
 			string content = http_client->sendRequest(*it);
 			id = parseDeviceId(content);
 			device.name = json->getParameterValuesFromContent("device", content);
+			device.page_version = json->getParameterValuesFromContent("version", content);
+			device.specification = device.name +"_"+ device.page_version;
 			device.ip = *it;
 			device.sensor.version = 1;
 			device.sensor.euid = id;
@@ -264,7 +266,7 @@ void VPTSensor::detectDevices(void) {
 			device.sensor.values.clear();
 			device.wake_up_time = VPT_DEFAULT_WAKEUP_TIME;
 			device.time_left = VPT_DEFAULT_WAKEUP_TIME;
-			log.information("VPT: Detected device " + device.name + " with ip " + device.ip);
+			log.information("VPT: Detected device " + device.name + " with ip " + device.ip + " and version of web page " + device.page_version);
 			map_devices.insert({id, device});
 		}
 		catch (...) {/* exceptions are cought in the caller */}
@@ -399,7 +401,7 @@ bool VPTSensor::createMsg(VPTDevice &device) {
 	string website = http_client->sendRequest(device.ip);
 
 	try {
-		device.sensor.values = json->getSensors(website);
+		device.sensor.values = json->getSensors(website, device.specification);
 	}
 	catch (Poco::Exception & exc) {
 		log.error("VPT: " + exc.displayText());
@@ -408,7 +410,7 @@ bool VPTSensor::createMsg(VPTDevice &device) {
 
 	convertPressure(device.sensor.values);
 	device.sensor.pairs = device.sensor.values.size();
-	device.sensor.device_id = json->getID(device.name);
+	device.sensor.device_id = json->getID(device.specification);
 	msg.device = device.sensor;
 	msg.time = time(NULL);
 	return true;
@@ -426,7 +428,7 @@ void VPTSensor::convertPressure(vector<Value> &values) {
 void VPTSensor::pairDevices(void) {
 	for(auto device = map_devices.begin(); device != map_devices.end(); device++) {
 		try {
-			json->loadDeviceConfiguration(device->second.name);
+			json->loadDeviceConfiguration(device->second.name, device->second.page_version);
 			updateTimestampOnVPT(device->second, ACTION_PAIR);
 		}
 		catch (Poco::Exception &e) {

JSON.h
@@ -69,7 +69,7 @@ class JSONDevices {
 public:
 	JSONDevices();
 
-	void loadDeviceConfiguration(std::string);
+	void loadDeviceConfiguration(std::string, std::string page_version);
 
 	bool isJSONFormat(std::string content);
 

VPT.h
@@ -36,6 +36,8 @@ extern bool quit_global_flag;
 typedef struct VPTDevice {
 	std::string name;
 	std::string ip;
+	std::string page_version;
+	std::string specification;
 	std::string password_hash;
 	Device sensor;
 	unsigned int wake_up_time;

commit f9f4d054687c3238cd278ee4bf0692bc5f658d4e

vpt: fix search of device specification
Support of different web pages versions requires to load the correct specification.
The name of a specification file is a concatenation of the target VPT's name and version.

This patch fixes setting of VPT's actuators which didn't work
after the specification version support was introduced.
Loading the specification file by device name only is not sufficient here.

Fixes: d376a71 ("vpt: add support for diffrent web pages")

Reported-by: Jan Viktorin <iviktorin@fit.vutbr.cz>
Signed-off-by: Lukas Koszegy <l.koszegy@gmail.com>
Acked-by: Jan Viktorin <iviktorin@fit.vutbr.cz>

JSON.cpp
@@ -75,6 +75,7 @@ bool JSONDevices::isJSONFormat(std::string content)
 std::string JSONDevices::generateRequestURL(string device_name, int id, float value)
 {
 	string request_url = "";
+	log.information("Generate request for device specification: " + device_name);
 	std::map<std::string,json_device>::iterator it = devices.find(device_name);
 
 	if (it == devices.end())

VPT.cpp
@@ -409,7 +409,7 @@ void VPTSensor::processCmdSet(Command cmd)
 	pair<int, float> value = cmd.values.at(0);
 	log.information("VPT: " + dev.ip + ": Set actuator with ID:" + to_string(value.first) + " on " + to_string((int)value.second));
 
-	string url_value = json->generateRequestURL(dev.name, value.first, value.second);
+	string url_value = json->generateRequestURL(dev.specification, value.first, value.second);
 	if (url_value.empty()) {
 		log.error("VPT: Setting actuator failed - device or actuator not found");
 		return;